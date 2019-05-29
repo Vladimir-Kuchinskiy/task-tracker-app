7b51fb349e0360d21513a5cda9f1e6973b68c10a
Vladimir-Kuchinskiy <kuchinskiy.wladimir@gmail.com>
2019-05-29 14:52:14 +0300

    Change configuration for production env in nginx configs and rails configs, add start.sh for starting backend server

--- todo-rails/config/environments/production.rb
+++ todo-rails/config/environments/production.rb
@@ -60,6 +60,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_cable.allowed_request_origins = [%r{http://*},
+                                                 %r{https://*}]
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
--- todo-rails/config/initializers/sidekiq.rb
+++ todo-rails/config/initializers/sidekiq.rb
@@ -3,9 +3,9 @@
 require 'sidekiq'
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}/0", namespace: "app3_sidekiq_#{Rails.env}" }
+  config.redis = { url: "#{ENV['REDIS_URL']}/0", namespace: "app3_sidekiq_#{Rails.env}" }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: "redis://#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}/0", namespace: "app3_sidekiq_#{Rails.env}" }
+  config.redis = { url: "#{ENV['REDIS_URL']}/0", namespace: "app3_sidekiq_#{Rails.env}" }
 end
